be7c21c1730f5ce969b1c266e320c5dc90095846
AzzarRizky <azzar.arza18@gmail.com>
2022-11-06 21:45:13 +0700

    add dialog Interface

--- app/src/main/java/com/azzar/datepicker/MainActivity.java
+++ app/src/main/java/com/azzar/datepicker/MainActivity.java
@@ -1,12 +1,15 @@
 package com.azzar.datepicker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private Button btnTanggal,btnKonfirmasi;
@@ -29,7 +32,16 @@ private TextView tvTanggal;
             }
         });
 
+        btnKonfirmasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tampilDialog();
+            }
+        });
+
     }
+
+
     public void processDatePickerResult(int year,int month,int day){
 
         String tahun = Integer.toString(year);
@@ -38,6 +50,28 @@ private TextView tvTanggal;
 
         String choosenDate = tanggal + " - " + bulan + " - " + tahun;
         tvTanggal.setText(choosenDate);
-
     }
+
+    private void tampilDialog(){
+        AlertDialog.Builder dialog= new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("Perhatian");
+        dialog.setMessage("Apakah Anda yakin data yang anda isi sudah benar?");
+
+        dialog.setPositiveButton("ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "TerimaKasih", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        dialog.setNegativeButton("tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 }
